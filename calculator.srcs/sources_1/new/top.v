4f5f8a310d449712a553ffb663905afabf90eddd
JuliusChoy <choyjulius@gmail.com>
2025-12-12 21:29:52 +0800

    i hope im just changing my local testing copy, i just wanna use the clock lol

--- calculator.srcs/sources_1/new/top.v
+++ calculator.srcs/sources_1/new/top.v
@@ -42,63 +42,8 @@ module top(
     wire [7:0]  ascii;
     wire        uart_ready;
 
-    reg         CLK50MHZ=0;
-    always @(posedge(clk))begin
-        CLK50MHZ<=~CLK50MHZ;
-    end
-    
-    ps2_receiver ps2_in (
-        .clk        (clk),
-        .kb_clk     (PS2Clk),
-        .kb_key     (PS2Data),
-        
-        .keycode    (keycode),
-        .valid      (valid_ps2)
-    );
-    
-    keys_2_calc(
-        .clk(clk),
-        .keycode(keycode),
-        .result(result),
-        .start(valid_ps2),        
-        .idle(alu_idle),
-                 
-        .A(A),    
-        .B(B),    
-        .op(op),    
-        .final(final),
-        .valid(valid_calc),       
-        .print(print)        
-    );
-    
-    // fake calculator
-    always@(posedge clk) begin
-        if(valid_calc) begin
-            alu_idle <= 0;
-            result <= A + B + op;
-        end else alu_idle <= 1;
-    end
-    
-    num_2_ascii converter (
-        .clk        (clk),
-        .num        (final),
-        .start      (print),
-        .uart_ready (uart_ready),
-        
-        .char       (ascii),
-        .uart_start (start)
-    );
-    
-    uart_sender uart_out (
-        .clk    (clk),
-        .start  (start),
-        .char   (ascii),
-        
-        .tx     (tx),
-        .ready  (uart_ready)
-    );
 
-        ALU ALU(
+    ALU ALU(
         .clk        (clk_300MHz),
         .rst        (reset),           // Added Reset
         .valid_in   (valid_in),   // Handshake: new operation is valid
